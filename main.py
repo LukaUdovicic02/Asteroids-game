fe254383158e810a5732cc2633872d09656bf10b
LukaUdovicic02 <lukaudovicic10@gmail.com>
2025-09-09 23:50:36 +0200

    feat: added the player and scene

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    p = Player(x,y)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+        p.update(dt)
         screen.fill("black")
+        p.draw(screen)
         pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
